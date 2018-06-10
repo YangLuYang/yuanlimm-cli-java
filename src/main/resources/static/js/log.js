7308efbfce4960152a2bd9b3badd13e45ac88f3f
Yu <GuryYu@qq.com>
2018-06-10 21:37:23 +0800

    M:优化许愿日志页面

--- src/main/resources/static/js/log.js
+++ src/main/resources/static/js/log.js
@@ -1,3 +1,5 @@
+var $wishLogList = $("#div_wish_log_list");
+
 (function () {
     getWishLogList();
     setInterval(function () {
@@ -6,42 +8,40 @@
 })();
 
 function getWishLogList() {
-    var $wishLogList = $("#div_wish_log_list");
     $wishLogList.empty();
     ajaxGet("/api/v1/wishLog/list", function (result) {
-        $.each(result.data, function (i, wishLog) {
-            console.log(wishLog);
-            if (wishLog.type === "stock") {
-                $wishLogList.prepend(
-                    $("<div>").addClass("item").append(
-                        $("<div>").addClass("row").append(
-                            $("<div>").addClass("col-md-6").append(
-                                $("<h3>").text("[股票] " + wishLog.stock.name)
-                            ).append(
-                                $("<h4>").addClass("organization").text(wishLog.amount)
-                            )
-                        ).append(
-                            $("<div>").addClass("col-md-6").append(
-                                $("<span>").addClass("period").text(new Date(wishLog.date).Format("yyyy-MM-dd hh:mm:ss"))
-                            )
-                        )
-                    ));
-            } else if (wishLog.type === "coin") {
-                $wishLogList.prepend(
-                    $("<div>").addClass("item").append(
-                        $("<div>").addClass("row").append(
-                            $("<div>").addClass("col-md-6").append(
-                                $("<h3>").text("[援力]")
-                            ).append(
-                                $("<h4>").addClass("organization").text(wishLog.amount / 100)
-                            )
-                        ).append(
-                            $("<div>").addClass("col-md-6").append(
-                                $("<span>").addClass("period").text(new Date(wishLog.date).Format("yyyy-MM-dd hh:mm:ss"))
-                            )
-                        )
-                    ));
-            }
-        });
+        if (result.data && result.data.length > 0) {
+            $.each(result.data, function (i, wishLog) {
+                console.log(wishLog);
+                if (wishLog.type === "stock") {
+                    add("[股票] " + wishLog.stock.name,
+                        wishLog.amount,
+                        new Date(wishLog.date).Format("yyyy-MM-dd hh:mm:ss"));
+                } else if (wishLog.type === "coin") {
+                    add("[援力]",
+                        wishLog.amount / 100,
+                        new Date(wishLog.date).Format("yyyy-MM-dd hh:mm:ss"));
+                }
+            });
+        } else {
+            add("[无数据]", "N/A", "N/A");
+        }
     });
+}
+
+function add(title, amount, date) {
+    $wishLogList.prepend(
+        $("<div>").addClass("item").append(
+            $("<div>").addClass("row").append(
+                $("<div>").addClass("col-md-6").append(
+                    $("<h3>").text(title)
+                ).append(
+                    $("<h4>").addClass("organization").text(amount)
+                )
+            ).append(
+                $("<div>").addClass("col-md-6").append(
+                    $("<span>").addClass("period").text(date)
+                )
+            )
+        ));
 }
